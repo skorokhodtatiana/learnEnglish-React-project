bd20f1deb8141a0bde14cf2a8effc203bc8c469b
skorokhodtatiana <tanjakharza@mail.ru>
2021-11-06 23:50:32 +0300

    added header components

--- learn-english/src/App.js
+++ learn-english/src/App.js
@@ -1,9 +1,14 @@
 import './assets/App.css';
+import Header from './components/Header';
+import logo from './assets/images/logo.png';
+
+
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Header logo={logo}></Header>
     </div>
   );
 }
