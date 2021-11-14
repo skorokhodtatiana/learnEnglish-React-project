4dcdfc2ee056bf23f7383053733f6867f0107eed
skorokhodtatiana <tanjakharza@mail.ru>
2021-11-14 23:19:29 +0300

    made layout footer, in process topicCards

--- learn-english/src/App.js
+++ learn-english/src/App.js
@@ -6,11 +6,75 @@ import TopicCard from "./components/topicCard/TopicCard";
 import FleshCard from "./components/fleshCard/FleshCard";
 import Footer from "./components/footer/Footer";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHamburger,
+  faUserFriends,
+  faCat,
+  faSuitcaseRolling,
+  faBriefcase,
+  faTree,
+} from "@fortawesome/free-solid-svg-icons";
+
+const food = (
+  <FontAwesomeIcon
+    icon={faHamburger}
+    className={"topic-picture food-purple"}
+  ></FontAwesomeIcon>
+);
+const family = (
+  <FontAwesomeIcon
+    icon={faUserFriends}
+    className={"topic-picture family-red"}
+  ></FontAwesomeIcon>
+);
+const animal = (
+  <FontAwesomeIcon
+    icon={faCat}
+    className={"topic-picture animal-orange"}
+  ></FontAwesomeIcon>
+);
+const travel = (
+  <FontAwesomeIcon
+    icon={faSuitcaseRolling}
+    className={"topic-picture travel-yellow"}
+  ></FontAwesomeIcon>
+);
+const work = (
+  <FontAwesomeIcon
+    icon={faBriefcase}
+    className={"topic-picture work-green"}
+  ></FontAwesomeIcon>
+);
+const nature = (
+  <FontAwesomeIcon
+    icon={faTree}
+    className={"topic-picture nature-blue"}
+  ></FontAwesomeIcon>
+);
+const topicCards = [
+  { id: 1, image: food, nameCard: "Food", size: 5 },
+  { id: 2, image: family, nameCard: "Family", size: 4 },
+  { id: 3, image: animal, nameCard: "Animals", size: 7 },
+  { id: 4, image: travel, nameCard: "Travel", size: 5 },
+  { id: 5, image: work, nameCard: "Work", size: 5 },
+  { id: 6, image: nature, nameCard: "Nature", size: 5 },
+];
+
 function App() {
   return (
     <div className="App">
       <Header logo={logo}></Header>
-      <TopicCard></TopicCard>
+      <div className="topicCard-wrapper">
+        {topicCards.map((card, id) => (
+          <TopicCard
+            key={card.id}
+            image={card.image}
+            nameCard={card.nameCard}
+            size={"(" + card.size + " cards)"}
+          ></TopicCard>
+        ))}
+      </div>
       <FleshCard></FleshCard>
       <Footer></Footer>
     </div>
--- learn-english/src/components/topicCard/TopicCard.jsx
+++ learn-english/src/components/topicCard/TopicCard.jsx
@@ -1,8 +1,9 @@
+import "./_topicCard.scss";
 const TopicCard = (props) => {
   const { image, nameCard, size, btnOpen } = props;
   return (
-    <div>
-      <img src={image} alt={nameCard}></img>
+    <div className="topicCard">
+      <div className="topicCard__picture">{image}</div>
       <div>{nameCard}</div>
       <div>{size}</div>
       <div>{btnOpen}</div>
