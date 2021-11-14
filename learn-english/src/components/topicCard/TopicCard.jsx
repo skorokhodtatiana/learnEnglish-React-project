252b2176732caff9f9cf996c1df5c78af44d12fc
skorokhodtatiana <tanjakharza@mail.ru>
2021-11-14 23:54:15 +0300

    made topicCards

--- learn-english/src/components/topicCard/TopicCard.jsx
+++ learn-english/src/components/topicCard/TopicCard.jsx
@@ -1,12 +1,13 @@
 import "./_topicCard.scss";
 const TopicCard = (props) => {
-  const { image, nameCard, size, btnOpen } = props;
+  const { image, nameCard, size } = props;
   return (
     <div className="topicCard">
       <div className="topicCard__picture">{image}</div>
-      <div>{nameCard}</div>
-      <div>{size}</div>
-      <div>{btnOpen}</div>
+      <div className={"topicCard__name textCard"}>{nameCard}</div>
+      <div className="textCard">{size}</div>
+
+      <button className="topicCard__btnOpen">Open</button>
     </div>
   );
 };
