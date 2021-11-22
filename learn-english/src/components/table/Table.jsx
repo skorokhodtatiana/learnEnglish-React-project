2e1f2b47c22e105fa23142f24a95da6de5aa379c
skorokhodtatiana <tanjakharza@mail.ru>
2021-11-22 13:43:55 +0300

    fixed bag

--- learn-english/src/components/table/Table.jsx
+++ learn-english/src/components/table/Table.jsx
@@ -20,7 +20,7 @@ const HeadTable = () => {
   );
 };
 
-const BodyTable = (props) => {
+const Table = (props) => {
   const { id, englishWord, transcription, russianWord, topic } = props;
 
   const [selectEdit, setselectEdit] = useState(false);
@@ -44,75 +44,70 @@ const BodyTable = (props) => {
     setchangeInput(e.target.value);
     console.log(e.target.value);
   };
-  return (
-    <tbody>
-      {selectEdit ? (
-        <tr>
-          <td>{id}</td>
-          <td>
-            <form action="#">
-              <input
-                type="text"
-                name="value"
-                placeholder={englishWord}
-                // value={englishWord}
-                onBlur={(e) => handleChange(e)}
-              ></input>
-            </form>
-          </td>
-          <td>
-            <form action="#">
-              <input
-                type="text"
-                name="value"
-                placeholder={transcription}
-                // value={transcription}
-                onBlur={(e) => handleChange(e)}
-              ></input>
-            </form>
-          </td>
-          <td>
-            <form action="#">
-              <input
-                type="text"
-                name="value"
-                placeholder={russianWord}
-                // value={russianWord}
-                onBlur={(e) => handleChange(e)}
-              ></input>
-            </form>
-          </td>
-          <td>
-            {/* <button onClick={() => saveChanges()}>Save</button> */}
-            <button>Cancel hange</button>
-          </td>
-        </tr>
-      ) : (
-        <tr>
-          <td>{id}</td>
-          <td>{englishWord}</td>
-          <td>{transcription}</td>
-          <td>{russianWord}</td>
-          <td>{topic}</td>
-          <td>
-            <button onClick={handleClickEdit}>
-              <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>
-            </button>
-            <button>
-              <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
-            </button>
-          </td>
-        </tr>
-      )}
-    </tbody>
-  );
-};
 
-const Table = (props) => {
   return (
     <table className="table">
       <HeadTable></HeadTable>
-      <BodyTable></BodyTable>
+      <tbody>
+        {selectEdit ? (
+          <tr>
+            <td>{id}</td>
+            <td>
+              <form action="#">
+                <input
+                  type="text"
+                  name="value"
+                  placeholder={englishWord}
+                  // value={englishWord}
+                  onBlur={(e) => handleChange(e)}
+                ></input>
+              </form>
+            </td>
+            <td>
+              <form action="#">
+                <input
+                  type="text"
+                  name="value"
+                  placeholder={transcription}
+                  // value={transcription}
+                  onBlur={(e) => handleChange(e)}
+                ></input>
+              </form>
+            </td>
+            <td>
+              <form action="#">
+                <input
+                  type="text"
+                  name="value"
+                  placeholder={russianWord}
+                  // value={russianWord}
+                  onBlur={(e) => handleChange(e)}
+                ></input>
+              </form>
+            </td>
+            <td>
+              {/* <button onClick={() => saveChanges()}>Save</button> */}
+              <button>Cancel hange</button>
+            </td>
+          </tr>
+        ) : (
+          <tr>
+            <td>{id}</td>
+            <td>{englishWord}</td>
+            <td>{transcription}</td>
+            <td>{russianWord}</td>
+            <td>{topic}</td>
+            <td>
+              <button onClick={handleClickEdit}>
+                <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+              </button>
+            </td>
+          </tr>
+        )}
+      </tbody>
     </table>
   );
 };
