8d62fa0d26ddcdaa02791180e8f6e3988aef1292
skorokhodtatiana <tanjakharza@mail.ru>
2021-11-25 08:27:58 +0300

    in the process of deleting, editing and saving changes

--- learn-english/src/components/table/BodyTable.jsx
+++ learn-english/src/components/table/BodyTable.jsx
@@ -10,85 +10,118 @@ const BodyTable = (props) => {
   const handleClickEdit = () => {
     setselectEdit(!false);
   };
+  const [changeInputEngWord, setchangeInputEngWord] = useState(englishWord);
+  const [changeInputTranscription, setchangeInputTranscription] =
+    useState(transcription);
+  const [changeInputRus, setchangeInputRus] = useState(russianWord);
 
-  const [changeInputEngWord, setchangeInputEngWord] = useState(props);
-  const handleChange = (e) => {
-    setchangeInputEngWord(e.target.value);
-    const newWord = { id: { id }, englishWord: { changeInputEngWord } };
-    const newArr = dataCards.push(newWord);
-    console.log(newArr);
+  const handleChangeEng = (eng) => {
+    setchangeInputEngWord(eng.target.value);
     console.log(changeInputEngWord);
+  };
+
+  const handleChangeTranscription = (tr) => {
+    setchangeInputTranscription(tr.target.value);
+    console.log(changeInputTranscription);
+  };
+
+  const handleChangeRus = (rus) => {
+    setchangeInputRus(rus.target.value);
+    console.log(changeInputRus);
+  };
+  const saveChanges = () => {
+    const newWord = {
+      id: id,
+      englishWord: changeInputEngWord,
+      transcription: changeInputTranscription,
+      russianWord: changeInputRus,
+      topic: topic,
+    };
+    console.log(newWord);
+    const newArr = [...dataCards];
+    // const newArr = dataCards.push(newWord);
+    newArr.push(newWord);
+    console.log(newArr);
   };
 
   const cancelChanges = () => {
     setselectEdit(false);
   };
 
-  return (
-    // <table className="table">
-    //   <tbody className="tbody">
-    selectEdit ? (
-      <tr>
-        <td>{id}</td>
-        <td>
-          <form action="#">
-            <input
-              type="text"
-              name="value"
-              placeholder={englishWord}
-              // value={englishWord}
-              onBlur={(e) => handleChange(e)}
-            ></input>
-          </form>
-        </td>
-        <td>
-          <form action="#">
-            <input
-              type="text"
-              name="value"
-              placeholder={transcription}
-              // value={transcription}
-              onBlur={(e) => handleChange(e)}
-            ></input>
-          </form>
-        </td>
-        <td>
-          <form action="#">
-            <input
-              type="text"
-              name="value"
-              placeholder={russianWord}
-              // value={russianWord}
-              onBlur={(e) => handleChange(e)}
-            ></input>
-          </form>
-        </td>
-        <td>
-          <button>Save</button>
-          {/* <button onClick={() => saveChanges()}>Save</button> */}
-          <button onClick={() => cancelChanges()}>Cancel change</button>
-        </td>
-      </tr>
-    ) : (
-      <tr>
-        <td>{id}</td>
-        <td>{englishWord}</td>
-        <td>{transcription}</td>
-        <td>{russianWord}</td>
-        <td>{topic}</td>
-        <td>
-          <button onClick={handleClickEdit} className="button">
-            <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>
-          </button>
-          <button className="button">
-            <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
-          </button>
-        </td>
-      </tr>
-    )
-    //   </tbody>
-    // </table>
+  const [oldListCards, setoldListCards] = useState(dataCards);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    console.log(oldListCards);
+    let newListCards = oldListCards.filter((el) => el.id !== id);
+    setoldListCards(newListCards);
+  };
+
+  return selectEdit ? (
+    <tr>
+      <td>{id}</td>
+      <td>
+        <form action="#">
+          <input
+            type="text"
+            name="value"
+            placeholder={englishWord}
+            // value={englishWord}
+            onChange={(eng) => handleChangeEng(eng)}
+          ></input>
+        </form>
+      </td>
+      <td>
+        <form action="#">
+          <input
+            type="text"
+            name="value"
+            placeholder={transcription}
+            // value={transcription}
+            onChange={(tr) => handleChangeTranscription(tr)}
+          ></input>
+        </form>
+      </td>
+      <td>
+        <form action="#">
+          <input
+            type="text"
+            name="value"
+            placeholder={russianWord}
+            // value={russianWord}
+            onChange={(rus) => handleChangeRus(rus)}
+          ></input>
+        </form>
+      </td>
+      <td>
+        <button>Save</button>
+        <button onClick={() => saveChanges()}>Save</button>
+        <button onClick={() => cancelChanges()}>Cancel change</button>
+      </td>
+    </tr>
+  ) : (
+    <tr>
+      <td>{id}</td>
+      <td>{englishWord}</td>
+      <td>{transcription}</td>
+      <td>{russianWord}</td>
+      <td>{topic}</td>
+      <td>
+        <button onClick={handleClickEdit} className="button">
+          <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>
+        </button>
+        <button
+          value={id}
+          onClick={(id) => handleDelete(id)}
+          className="button"
+        >
+          <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+        </button>
+      </td>
+    </tr>
   );
+  //   </tbody>
+  // </table>
 };
 
 export default BodyTable;
